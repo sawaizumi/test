9fc6a74502b48b1060edf82620fcbe3a192a4b19
sawaizumi <maniachousecom@yahoo.co.jp>
2014-11-16 13:42:24 +0900

    test

--- select.php
+++ select.php
@@ -19,8 +19,21 @@
 }
 function Main( $arStrings_Argument )
 {
+	$eString_Debug = "";
+
 	$eString_JSON = "{\"stations\":[{\"id\": 9931109, \"name\": \"テレコムセンター\", \"lat\": 35.617593, \"lon\": 139.779327}]}";
 	$eJSON = json_decode( $eString_JSON );
+
+	$eString_Latitude = "35.617593";
+	$eString_Longitude = "139.779327";
+	if ( $_REQUEST["lat"] )
+	{
+		$eString_Latitude = $_REQUEST["lat"];
+	}
+	if ( $_REQUEST["lon"] )
+	{
+		$eString_Longitude = $_REQUEST["lon"];
+	}
 
 	try
 	{
@@ -36,8 +49,39 @@ function Main( $arStrings_Argument )
 			$arStrings[] = $eRow;
 		}
 
-		$eString_Debug = "";
-		$eString_Debug .= print_r( $arStrings, TRUE );
+		$eString_SQL = "SELECT * FROM `d__train`.`t__station` WHERE `c__lon` > ( ? - 0.02 ) AND `c__lon` < ( ? + 0.02 ) AND `c__lat` > ( ? - 0.02 ) AND `c__lat` < ( ? + 0.02 );";
+		$arArguments_SQL = array();
+		$arArguments_SQL[] = $eString_Longitude;
+		$arArguments_SQL[] = $eString_Longitude;
+		$arArguments_SQL[] = $eString_Latitude;
+		$arArguments_SQL[] = $eString_Latitude;
+		$eStatement = $eDB->prepare( $eString_SQL );
+		$eStatement->execute( $arArguments_SQL );
+		$arStrings = array();
+		$arStrings_Distance = array();
+		$arStrings_Debug = array();
+		while ( $eRow = $eStatement->fetch( PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT ) )
+		{
+			$rDistance = ( $eString_Longitude - $eRow["c__lon"] ) * ( $eString_Longitude - $eRow["c__lon"] ) + ( $eString_Latitude - $eRow["c__lat"] ) * ( $eString_Latitude - $eRow["c__lat"] );
+			$eRow["distance"] = $rDistance;
+			$arStrings_Distance[] = $rDistance;
+			$arStrings[] = $eRow;
+		}
+		$arStrings_Sorted = $arStrings;
+		array_multisort( $arStrings_Distance, $arStrings_Sorted, SORT_NUMERIC );
+		$arStations = array();
+		foreach ( $arStrings_Sorted as $eRow )
+		{
+			$aaStation = array();
+			$aaStation["id"] = $eRow["c__station_cd"];
+			$aaStation["name"] = $eRow["c__station_name"];
+			$aaStation["lat"] = $eRow["c__lat"];
+			$aaStation["lon"] = $eRow["c__lon"];
+			$arStations[] = $aaStation;
+		}
+		$eJSON->stations = $arStations;
+
+/*		$eString_Debug .= print_r( $arStrings, TRUE );
 		$eString_Debug .= "<br />";
 
 		$eString_SQL = "SELECT COUNT( * ) FROM `d__train`.`t__station`;";
@@ -52,7 +96,7 @@ function Main( $arStrings_Argument )
 
 		$eString_Debug .= print_r( $arStrings, TRUE );
 		$eString_Debug .= "<br />";
-
+*/
 	}
 	catch ( PDOException $e )
 	{
