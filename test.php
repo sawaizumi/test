97900f736f5ac7ad6200bc30ad1eda9aba5229ee
sawaizumi <maniachousecom@yahoo.co.jp>
2014-11-16 12:50:34 +0900

    test

--- test.php
+++ test.php
@@ -19,7 +19,7 @@
 }
 function Main( $arStrings_Argument )
 {
-	$eString_JSON = "{\"stations\":[{\"id\": 9931109, \"name\": \"テレコムセンター\", \"lat\", \"\": 35.617593, \"lon\": 139.779327}	]}";
+	$eString_JSON = "{\"stations\":[{\"id\": 9931109, \"name\": \"テレコムセンター\", \"lat\": 35.617593, \"lon\": 139.779327}]}";
 	header( "Content-type: text/html; charset=UTF-8" );
 	echo "{\"stations\":false}";
 }
