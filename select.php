40386b9fc0fb490968f91a409ad04f55a2e74fd9
sawaizumi <maniachousecom@yahoo.co.jp>
2014-11-16 13:29:05 +0900

    test

--- select.php
+++ select.php
@@ -22,7 +22,7 @@ function Main( $arStrings_Argument )
 	$eString_JSON = "{\"stations\":[{\"id\": 9931109, \"name\": \"テレコムセンター\", \"lat\": 35.617593, \"lon\": 139.779327}]}";
 //	$eJSON = json_decode( "{\"analyze_request\":false}" );
 //	$eJSON = json_decode( $eString_JSON );
-/*
+
 	try
 	{
 		$eDB = new PDO( "mysql:dbname=d__train;host=tcth2014-den2.cloudapp.net", "test", "password" );
@@ -59,72 +59,10 @@ function Main( $arStrings_Argument )
 	{
 		$eJSON->error_message = $e->getMessage();
 	}
-*/
+
 //	$eString_JSON = json_encode( $eJSON );
 	header( "Content-type: text/html; charset=UTF-8" );
 	echo $eString_JSON;
-}
-
-
-function local_JSON_Encode( $eJSON )
-{
-	if ( is_array( $eJSON ) )
-	{
-		$eString_JSON = local_JSON_Encode_Array( $eJSON );
-	}
-	else
-	{
-		$eString_JSON = "{" . $eJSON . "}";
-	}
-
-	return $eString_JSON;
-}
-
-
-function local_JSON_Encode_Array( $eJSON )
-{
-	if ( array_diff_key( $eJSON, array_keys( array_keys( $eJSON ) ) ) )
-	{
-		$eString_JSON = local_JSON_Encode_Assoc( $eJSON );
-	}
-	else
-	{
-		$eString_JSON = "[";
-		foreach ( $eJSON as $eValue )
-		{
-			if ( is_array( $eJSON ) )
-			{
-				$eString_JSON = local_JSON_Encode_Array( $eValue ), ",";
-			}
-			else
-			{
-				$eString_JSON .= $eValue . ",";
-			}
-		}
-		$eString_JSON .= "]";
-	}
-
-	return $eString_JSON;
-}
-
-
-function local_JSON_Encode_Assoc( $eJSON )
-{
-	$eString_JSON = "{";
-	foreach ( $eJSON as $eKey => $eValue )
-	{
-		if ( is_array( $eJSON ) )
-		{
-			$eString_JSON = "\"" . $eKey . "\":" . local_JSON_Encode_Array( $eValue ), ",";
-		}
-		else
-		{
-			$eString_JSON .= "\"" . $eKey . "\":" . $eValue . ",";
-		}
-	}
-	$eString_JSON .= "}";
-
-	return $eString_JSON;
 }
 
 
