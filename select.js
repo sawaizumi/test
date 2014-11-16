cf09360a49b5118856c8d32ec3a4804d230bf5ad
sawaizumi <maniachousecom@yahoo.co.jp>
2014-11-16 11:53:36 +0900

    test

---
+++ select.js
@@ -0,0 +1,183 @@
+// 日本語UTF-8, LF
+
+
+// global
+var g_eString_FormName = "test";
+var g_eString_URL_Default = "http://tcth2014-den-test.azurewebsites.net/select.php";
+
+
+// -------------------------------------------------------------------
+// event ( onclick )
+
+function local_OnLoad()
+{
+	if ( navigator.geolocation )
+	{
+		navigator.geolocation.getCurrentPosition( callback_getCurrentPosition__Success, callback_getCurrentPosition__Failure );
+	}
+	else
+	{
+		alert( "navigator.geolocation : false" );
+	}
+
+	function callback_getCurrentPosition__Success( ePosition )
+	{
+		eSender = 
+		{
+			positions : ePosition,
+		};
+
+		local_SendRequest_JSON( eSender, callback_SendRequest_JSON__Success );
+	}
+	function callback_getCurrentPosition__Failure( eError )
+	{
+		alert( eError.code );
+	}
+	function callback_SendRequest_JSON__Success( eXMLHttpRequest )
+	{
+		try
+		{
+			eReceiver = JSON.parse( eXMLHttpRequest.responseText );
+			alert( eXMLHttpRequest.responseText );
+
+			var eElement;
+
+			eElement = document.getElementById( "id__select__line" );
+			if ( eElement )
+			{
+				eElement.options[i] = new Option( oname, ovalue );
+			}
+		}
+		catch ( eError )
+		{
+			alert( eError.code );
+			alert( eXMLHttpRequest.responseText );
+		}
+	}
+}
+
+
+// -------------------------------------------------------------------
+// event ( onclick )
+
+function local_OnClick( eString_Submit )
+{
+	if ( navigator.geolocation )
+	{
+		navigator.geolocation.getCurrentPosition( callback_getCurrentPosition__Success, callback_getCurrentPosition__Failure );
+		document[g_eString_FormName].test.value = eString_Submit;
+	}
+	else
+	{
+		alert( "navigator.geolocation : false" );
+		return false;
+	}
+
+	function callback_getCurrentPosition__Success( ePosition )
+	{
+		document[g_eString_FormName].position_latitude.value = ePosition.coords.latitude;
+		document[g_eString_FormName].position_longitude.value = ePosition.coords.longitude;
+		document[g_eString_FormName].position_altitude.value = ePosition.coords.altitude;
+		document[g_eString_FormName].position_accuracy.value = ePosition.coords.accuracy;
+		document[g_eString_FormName].position_altitudeaccuracy.value = ePosition.coords.altitudeAccuracy;
+		document[g_eString_FormName].position_heading.value = ePosition.coords.heading;
+		document[g_eString_FormName].position_speed.value = ePosition.coords.speed;
+	}
+	function callback_getCurrentPosition__Failure( eError )
+	{
+		alert( eError.code );
+	}
+}
+
+
+// -------------------------------------------------------------------
+// send request
+
+function local_SendRequest_JSON( eSender, fpCallBack__Succeed, fpCallBack__Failure, eString_URL )
+{
+	if ( eSender == undefined )
+	{
+		eSender = 
+		{
+			mode : "get",
+			category : "test",
+		};
+	}
+
+	eString_Sender = "json=";
+	eString_Sender += encodeURIComponent( JSON.stringify( eSender ) );
+	eString_Sender += "&json_send=true";
+
+	local_SendHttpRequest_Asynchronous( eString_Sender, fpCallBack__Succeed, fpCallBack__Failure, eString_URL );
+}
+
+
+function local_SendHttpRequest_Asynchronous( eString_Sender, fpCallBack__Succeed, fpCallBack__Failure, eString_URL )
+{
+	var eXMLHttpRequest;
+
+	if ( fpCallBack__Succeed == undefined )
+	{
+		fpCallBack__Succeed = callback_SendHttpRequest_Asynchronous__Succeed;
+	}
+	if ( fpCallBack__Failure == undefined )
+	{
+		fpCallBack__Failure = callback_SendHttpRequest_Asynchronous__Failure;
+	}
+	if ( eString_URL == undefined )
+	{
+		eString_URL = g_eString_URL_Default;
+	}
+
+	eXMLHttpRequest = new XMLHttpRequest();
+	eXMLHttpRequest.abort();
+	eXMLHttpRequest.open( "POST", eString_URL, true );
+	eXMLHttpRequest.onreadystatechange = callback_SendHttpRequest_Asynchronous;
+	eXMLHttpRequest.setRequestHeader( "Content-Type", "application/x-www-form-urlencoded" );
+	eXMLHttpRequest.send( eString_Sender );
+
+
+	function callback_SendHttpRequest_Asynchronous()
+	{
+		if ( eXMLHttpRequest.readyState == 4 )
+		{
+			if ( eXMLHttpRequest.status == 200 || eXMLHttpRequest.status == 201)
+			{
+				// リクエストの処理
+				fpCallBack__Succeed( eXMLHttpRequest );
+			}
+			else
+			{
+				// エラー処理
+				fpCallBack__Failure( eXMLHttpRequest );
+			}
+		}
+	}
+
+
+	function callback_SendHttpRequest_Asynchronous__Succeed( eXMLHttpRequest )
+	{
+		var eElement;
+
+		eElement = document.getElementById( "id__debug" );
+		if ( eElement )
+		{
+			eElement.innerHTML = eXMLHttpRequest.responseText;
+		}
+	}
+
+
+	function callback_SendHttpRequest_Asynchronous__Failure( eXMLHttpRequest )
+	{
+		var eElement;
+
+		eElement = document.getElementById( "id__debug" );
+		if ( eElement )
+		{
+			eElement.innerHTML = "error.<br />\n";
+			eElement.innerHTML += eString_Sender;
+		}
+	}
+}
+
+
---
+++ select.php
@@ -0,0 +1,121 @@
+<?php
+// 日本語UTF-8, LF
+
+
+// include
+
+
+// define
+
+
+// global
+
+
+// main
+{
+	Main( $argv );
+
+	exit;
+}
+function Main( $arStrings_Argument )
+{
+	$eString_JSON = $_REQUEST( "json" );
+	$eJSON = json_decode( rawurldecode( $eString_JSON ) );
+
+	try
+	{
+		$eDB = new PDO( "mysql:dbname=d__train;host=tcth2014-den2.cloudapp.net", "test", "password" );
+
+		$eString_SQL = "SET NAMES utf8;";
+		$arArguments_SQL = array();
+		$eStatement = $eDB->prepare( $eString_SQL );
+		$eStatement->execute( $arArguments_SQL );
+		$arStrings = array();
+		while ( $eRow = $eStatement->fetch( PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT ) )
+		{
+			$arStrings[] = $eRow;
+		}
+
+		$eString_Debug = "";
+		$eString_Debug .= print_r( $arStrings, TRUE );
+		$eString_Debug .= "<br />";
+
+		$eString_SQL = "SELECT COUNT( * ) FROM `d__train`.`t__station`;";
+		$arArguments_SQL = array();
+		$eStatement = $eDB->prepare( $eString_SQL );
+		$eStatement->execute( $arArguments_SQL );
+		$arStrings = array();
+		while ( $eRow = $eStatement->fetch( PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT ) )
+		{
+			$arStrings[] = $eRow;
+		}
+
+		$eString_Debug .= print_r( $arStrings, TRUE );
+		$eString_Debug .= "<br />";
+
+		if ( $_REQUEST[latitude] )
+		{
+			$eString_Latitude = $_REQUEST[latitude];
+		}
+		else
+		{
+			$eString_Latitude = "35.617593";
+		}
+		if ( $_REQUEST[longitude] )
+		{
+			$eString_Longitude = $_REQUEST[longitude];
+		}
+		else
+		{
+			$eString_Longitude = "139.779327";
+		}
+
+		$eString_SQL = "SELECT * FROM `d__train`.`t__station` WHERE `c__lon` > ( ? - 0.02 ) AND `c__lon` < ( ? + 0.02 ) AND `c__lat` > ( ? - 0.02 ) AND `c__lat` < ( ? + 0.02 );";
+		$arArguments_SQL = array();
+		$arArguments_SQL[] = $eString_Longitude;
+		$arArguments_SQL[] = $eString_Longitude;
+		$arArguments_SQL[] = $eString_Latitude;
+		$arArguments_SQL[] = $eString_Latitude;
+		$eStatement = $eDB->prepare( $eString_SQL );
+		$eStatement->execute( $arArguments_SQL );
+		$arStrings = array();
+		$arStrings_Distance = array();
+		$arStrings_Debug = array();
+		while ( $eRow = $eStatement->fetch( PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT ) )
+		{
+			$arStrings_Debug[] = $eRow["c__station_name"];
+			$rDistance = ( $eString_Longitude - $eRow["c__lon"] ) * ( $eString_Longitude - $eRow["c__lon"] ) + ( $eString_Latitude - $eRow["c__lat"] ) * ( $eString_Latitude - $eRow["c__lat"] );
+			$eRow["distance"] = $rDistance;
+			$arStrings_Distance[] = $rDistance;
+			$arStrings[] = $eRow;
+		}
+
+		$eString_Debug .= print_r( $arStrings_Debug, TRUE );
+		$eString_Debug .= "<br />";
+		$arStrings_Sorted = $arStrings;
+		array_multisort( $arStrings_Distance, $arStrings_Sorted, SORT_NUMERIC );
+		$eString_Debug .= print_r( $arStrings_Sorted, TRUE );
+		$eString_Debug .= "<br />";
+
+		$arStrings_Station = array();
+		foreach ( $arStrings_Sorted as $eRow )
+		{
+			$arStrings_Station[] = $eRow["c__station_name"];
+		}
+
+		$eJSON["stations"] = $arStrings_Station;
+
+		$eJSON["debug"] = $eString_Debug;
+	}
+	catch ( PDOException $e )
+	{
+		$eJSON["error"] = $e->getMessage();
+	}
+
+	$eString_JSON = json_encode( $eJSON );
+	header( "Content-type: text/html; charset=UTF-8" );
+	echo $g_eString_JSON;
+}
+
+
+?>
